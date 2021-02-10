e4cfa4c455a78446a85b597f9f733b0a1129fa0a
bsj0472 <b_s_j0472@naver.com>
2021-02-10 09:15:53 +0900

    commit

--- app/src/main/java/com/b_s_j/inlove/Login.java
+++ app/src/main/java/com/b_s_j/inlove/Login.java
@@ -3,12 +3,19 @@ package com.b_s_j.inlove;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Btn_login(View view) {
+    }
+
+    public void Btn_logingoogle(View view) {
     }
 }
